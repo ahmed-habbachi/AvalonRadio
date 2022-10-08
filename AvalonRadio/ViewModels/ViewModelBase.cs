765ff9a450ee6ddee0051b7406d2eff64e19f256
Ahmed HABBACHI <a.habbachi@case-tunisia.com>
2022-10-08 18:24:04 +0100

    Add project files.

---
+++ AvalonRadio/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvalonRadio.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
